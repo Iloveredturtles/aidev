a531725025b43a6fa47deb430745124a41d2129a
Iloveredturtles <165796065+Iloveredturtles@users.noreply.github.com>
2025-08-13 18:05:43 +0700

    meeting1
    meeting1

---
+++ meeting1/meeting1.py
@@ -0,0 +1,6 @@
+
+name = input("Name : ") #menu price value based on input
+last_name = input("Last Name : ")
+class_name = input("Class Name : ")
+info= name + last_name + class_name #Purchase totals
+print("Your Information : " + info )
